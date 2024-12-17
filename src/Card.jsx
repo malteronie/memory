95d26e7d153608fa1f3fa0ea0de9a344bc3b50eb
malteronie <153739321+malteronie@users.noreply.github.com>
2024-12-17 09:55:57 +0100

    maj

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,56 @@
 //Définir une carte
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './card.css';
 
-export default function Card ({value, etat }) {
+export default function Card ({value, etat, trouve}) {
+    const [showCard, setVal] = useState(etat)
+    var test = false
+    const clickButton=()=>{
+        setVal(showCard ===false ? true:false);
+        
+    }
+    // const isMatched=()=>{
+       useEffect(()=>{
+            if (showCard===true){
+                if (test==true){
+                    console.log("salut")
+                    test = false
+                } else {
+                    test=true
+                    console.log("ok")
+                }
+                console.log(value)
+                
+            } else {
+                console.log("hi")
+            }
+        })
+    // }
+
+
     return (
         <div className="card">
             <div>{value}</div>
-            {etat}
+            <button onClick={() => clickButton()}>{showCard} coucou </button>
         </div>
     )
 }
+
+
+
+/*
+créer une variable (showCard) carte retourné
+si carte retourné
+vérifier la variable carte retourné
+vérifier s'il y a déjà une carte retourné
+si showCard = true
+    récupéré dans la liste la carte avec l'état true dans une variable
+    récupérer la valeur de la carte cliqué
+    comparer la valeur des 2 cartes
+        Si les cartes ont la même valeur:
+changer le match (la valeur des 2 cartes) en True
+vérifier si c'et la dernière paire à trouver
+    sinon
+changer le stat des 2 cartes en false
+  réinitialiser showCard = false
+*/
--- src/CardList.jsx
+++ src/CardList.jsx
@@ -1,22 +1,26 @@
 //Définir la liste des cartes
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import './card.css';
 
-const cardValues = [1,2,3,4,5]
+const cardValues = ([1,2,3,4,5])
 const doubleCard= [...cardValues, ...cardValues]
 
+
 function List () {
+    
     return (
         <div className='card-list'>
             {doubleCard.map((element,index) => (
                 <div className='color-card' key={element.id}>
-                    <Card key={index} value={element}/>
+                    <Card key={index} value={element} etat={false} trouve={false}/>
                 </div>
            )).sort(() => Math.random() - 0.5)}
+           
         </div>
    );
+   
 }
 
 export default List;
